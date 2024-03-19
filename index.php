bc673028fd7d7150c6dc2c497933d7d3cfeab112
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-03-19 16:54:07 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!-- 
+Milestone 1
+Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere maiuscole, lettere minuscole, numeri e simboli) da restituire all’utente.
+Scriviamo tutto (logica e layout) in un unico file index.php
+Milestone 2
+Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale
+Milestone 3 (BONUS)
+Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite $_SESSION recupererà la password da mostrare all’utente.
+Milestone 4 (BONUS)
+Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli. Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme).
+Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali. -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Password-Generator</title>
+</head>
+
+
+<body>
+
+<h1>Password-Generator</h1>
+
+
+<body>
+    <div class="container">
+        <form>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" name="password" id="password" placeholder="" />
+            </div>
+            <button type="submit" class="btn btn-primary">Send</button>
+        </form>
+        </div>
+    </div>
+
+<!-- Bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
